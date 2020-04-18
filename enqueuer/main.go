d46c044a55530a6027b750045770b6081c2e8a2d
Dany M Pradana <danympradana@gmail.com>
2020-04-18 22:25:35 +0700

    Add gin server

--- enqueuer/main.go
+++ enqueuer/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/gocraft/work"
 	"github.com/gomodule/redigo/redis"
 )
@@ -18,9 +25,76 @@ var redisPool = &redis.Pool{
 
 var enqueuer = work.NewEnqueuer("go_scheduler_namespace", redisPool)
 
-func main() {
-	_, err := enqueuer.Enqueue("greeting", work.Q{"name": "Dany M Pradana", "message": "How are you?"})
+type Handler struct {
+	Enqueuer *work.Enqueuer
+}
+
+func NewHandler(enqueuer *work.Enqueuer) Handler {
+	return Handler{
+		Enqueuer: enqueuer,
+	}
+}
+
+type GreetingRequest struct {
+	Name    string `json:"name"`
+	Message string `json:"message"`
+}
+
+func (h Handler) EnqueueGreeting(c *gin.Context) {
+	var req GreetingRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if req.Name == "" || req.Message == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"status": "Name and Message are mandatory"})
+		return
+	}
+	_, err := enqueuer.Enqueue("greeting", work.Q{"name": req.Name, "message": req.Message})
 	if err != nil {
 		log.Fatal(err)
+		c.JSON(http.StatusInternalServerError, gin.H{"status": "Oops sorry try again"})
+		return
 	}
+	c.JSON(http.StatusOK, gin.H{"status": "Thanks for send me message"})
+}
+
+func main() {
+	router := gin.Default()
+	handler := NewHandler(enqueuer)
+	v1 := router.Group("/api/v1")
+	{
+		v1.POST("/enqueue", handler.EnqueueGreeting)
+	}
+	srv := &http.Server{
+		Addr:    ":4500",
+		Handler: router,
+	}
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shuting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/danymarita/go-scheduler
 go 1.13
 
 require (
+	github.com/gin-gonic/gin v1.6.2
 	github.com/gocraft/work v0.5.1
 	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/robfig/cron v1.2.0 // indirect
